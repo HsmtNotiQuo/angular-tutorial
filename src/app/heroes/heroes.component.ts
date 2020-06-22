e05dec91cd43bf5f4d82f6fb9c77475d2801ea87
HsmtNotiQuo <hsmt@notiquo.com>
2020-06-22 16:16:11 +0900

    end part5

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,12 +10,10 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(
     private heroService: HeroService,
-    private messageService: MessageService
   ) { }
 
   ngOnInit(): void {
@@ -25,10 +23,5 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
 }
